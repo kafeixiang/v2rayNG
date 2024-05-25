e83cbe7d16bd4836be4b42611dc19c5a4a10a6ac
kafeixiang <79391973+kafeixiang@users.noreply.github.com>
2024-05-25 10:03:38 +0800

    Update MainActivity.kt

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/MainActivity.kt
@@ -675,9 +675,6 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
             R.id.user_asset_setting -> {
                 startActivity(Intent(this, UserAssetActivity::class.java))
             }
-            R.id.promotion -> {
-                Utils.openUri(this, "${Utils.decode(AppConfig.PromotionUrl)}?t=${System.currentTimeMillis()}")
-            }
             R.id.logcat -> {
                 startActivity(Intent(this, LogcatActivity::class.java))
             }
