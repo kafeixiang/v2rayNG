24d53e24b5634ef28deb42714f4a22800602aec3
kafeixiang <tingfengyuwo@gmail.com>
2025-04-04 15:26:42 +0800

    qg
    mm
    
    覆盖

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -691,7 +691,6 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                     .putExtra("isRunning", mainViewModel.isRunning.value == true)
             )
 
-            R.id.promotion -> Utils.openUri(this, "${Utils.decode(AppConfig.APP_PROMOTION_URL)}?t=${System.currentTimeMillis()}")
             R.id.logcat -> startActivity(Intent(this, LogcatActivity::class.java))
             R.id.check_for_update -> startActivity(Intent(this, CheckUpdateActivity::class.java))
             R.id.about -> startActivity(Intent(this, AboutActivity::class.java))
